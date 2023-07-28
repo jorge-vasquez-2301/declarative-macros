2a0ecf3af5132ba8eda14d005f903c06bbed9269
Jorge Vasquez <jv2301@gmail.com>
2023-07-28 15:14:23 -0400

    improve newtype macro

--- src/bin/newtypes.rs
+++ src/bin/newtypes.rs
@@ -1,36 +1,34 @@
-struct FirstName {
-    value: String,
-}
+macro_rules! newtype {
+    ($struct_type:ident, |$name:ident: &$internal_type:ty| $predicate:expr) => {
+        struct $struct_type {
+            value: $internal_type,
+        }
 
-struct LastName {
-    value: String,
-}
-
-struct Age {
-    value: i32,
-}
-
-struct Pay {
-    value: i32,
-}
-
-macro_rules! generate_get_value {
-    ($struct_type:ident) => {
-        generate_get_value!($struct_type, String);
-    };
-    ($struct_type:ident,$return_type:ident) => {
         impl $struct_type {
-            pub fn get_value(&self) -> &$return_type {
+            pub fn get_value(&self) -> &$internal_type {
                 &self.value
+            }
+        }
+
+        impl TryFrom<$internal_type> for $struct_type {
+            type Error = &'static str;
+
+            fn try_from(value: $internal_type) -> Result<Self, Self::Error> {
+                let f = |$name: &$internal_type| $predicate;
+                if f(&value) {
+                    Ok(Self { value })
+                } else {
+                    Err("Predicate failed")
+                }
             }
         }
     };
 }
 
-generate_get_value!(FirstName);
-generate_get_value!(LastName);
-generate_get_value!(Age, i32);
-generate_get_value!(Pay, i32);
+newtype!(FirstName, |str: &String| !str.is_empty());
+newtype!(LastName, |str: &String| !str.is_empty());
+newtype!(Age, |v: &i32| v > &0);
+newtype!(Pay, |v: &i32| v > &0);
 
 fn main() {
     todo!()
