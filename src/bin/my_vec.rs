594f9255ce069d67b02f6f1624727a612b3ed14f
Jorge Vasquez <jv2301@gmail.com>
2023-07-28 14:55:42 -0400

    Support trailing commas in my_vec macro

--- src/bin/my_vec.rs
+++ src/bin/my_vec.rs
@@ -5,14 +5,14 @@ macro_rules! my_vec {
     (make an empty vec) => (
         Vec::new()
     );
-    {$x:expr} => {
+    {$x:expr$(,)?} => {
         {
             let mut v = Vec::new();
             v.push($x);
             v
         }
     };
-    [$($x:expr),+] => (
+    [$($x:expr),+$(,)?] => (
         {
             let mut v = Vec::new();
             $(
@@ -28,6 +28,6 @@ fn main() {
     println!("{:?}", empty);
     let also_empty: Vec<i32> = my_vec!(make an empty vec);
     println!("{:?}", also_empty);
-    let three_numbers = my_vec!(1, 2, 3);
+    let three_numbers = my_vec!(1, 2, 3,);
     println!("{:?}", three_numbers);
 }
